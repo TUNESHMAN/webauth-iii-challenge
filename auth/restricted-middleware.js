3341958a0bbfa1d2e89203cc822604c0a4e36a85
Babatunde <babatundea15@gmail.com>
2020-01-29 16:24:00 +0100

    I fixed bugg in the restricted middleware file

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,23 +1,27 @@
 const bcrypt = require("bcryptjs");
 const Users = require("../users/user-model");
 module.exports = (req, res, next) => {
-    const { username, password } = req.headers;
-  
-    if (username && password) {
-      Users.findBy({ username })
-        .first()
-        .then(user => {
-          if (user && bcrypt.compareSync(password, user.password)) {
-            next();
-          } else {
-            res.status(401).json({ message: 'Credentials are wrong' });
-          }
-        })
-        .catch(error => {
-          res.status(500).json({ message: 'Ran into an unexpected error' });
-        });
-    } else {
-      res.status(400).json({ message: 'No valid credentials provided' });
-    }
-  };
-  
+  const token = req.headers.authorization;
+  if (token) {
+    // verify is it valid?
+    // it's made by us (was made using the secret)
+    // the token hasn't expired
+    // hasn't been tampered with etc
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || 'thesecret',
+      // callback (err, decodedToken)
+      (err, decoded) => {
+        if (err) {
+          res.status(401).json({ message: 'token bad' })
+        } else {
+          // let's put the decoded token on req
+          req.decodedToken = decoded;
+          next();
+        }
+      }
+    )
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
+};
